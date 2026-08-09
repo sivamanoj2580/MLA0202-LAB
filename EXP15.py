05cf73c806ec895cbdc3260a8fad4a0d95ff7714
chenna kesava <chennakesavareddyusthelamuri@gmail.com>
2026-08-09 20:22:07 +0530

    Add files via upload

---
+++ EXP15.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+model = GaussianNB()
+model.fit(X_train,y_train)
+
+pred = model.predict(X_test)
+
+print("Accuracy:",accuracy_score(y_test,pred))
+
+print("\nConfusion Matrix")
+print(confusion_matrix(y_test,pred))
+
+sample = [[5.1,3.5,1.4,0.2]]
+
+result = model.predict(sample)
+
+print("Predicted Flower:",iris.target_names[result[0]])
+
+print("U. Lakshmi Chenna Kesava Reddy - 192425206")
