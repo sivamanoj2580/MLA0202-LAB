d8e60514ac3eeeaffb283794bf74fefae142bfe5
chenna kesava <chennakesavareddyusthelamuri@gmail.com>
2026-08-18 11:58:23 +0530

    Add files via upload

---
+++ EXP 18.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris=load_iris()
+X,y=iris.data,iris.target
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+model=Perceptron()
+model.fit(X_train,y_train)
+pred=model.predict(X_test)
+
+print("Accuracy:",accuracy_score(y_test,pred))
+print("Predicted Flower:",iris.target_names[model.predict([[5.1,3.5,1.4,0.2]])[0]])
+print("U. Lakshmi Chenna Kesava Reddy - 192425206")
